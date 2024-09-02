d2436b0d32e03001368b495b052cfb04222e45aa
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2024-09-02 23:05:54 +0200

    Added DbContext

---
+++ DataAccessLayer/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataAccessLayer.Models.Domain;
+
+namespace DataAccessLayer.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
