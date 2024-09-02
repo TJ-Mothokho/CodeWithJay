e5f8f3a9219299231c00e7b474e373ea923fdc13
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2024-09-02 22:51:39 +0200

    Added Domain Models

---
+++ DataAccessLayer/Models/Domain/BlogPost.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLayer.Models.Domain
+{
+    public class BlogPost
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Content { get; set; }
+        public string UrlHandle { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Author { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
